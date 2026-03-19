d67604761c98b9a72aa70559e298ada07df1fafe
edwingsantos <edwing.santos@ucas-edu.net>
2026-03-19 09:52:40 -0600

    starteed in my create shapes

---
+++ create_shapes.py
@@ -0,0 +1,10 @@
+#creation for the shapes 
+
+
+#make a funtion called creation 
+
+#make a while true loop in it
+
+    #print the avalible shapes numbered 
+
+    #make the user input what their shape is 
---
+++ main.py
@@ -0,0 +1,31 @@
+#ES 1rst geomarty calculator
+
+#print the greating
+print("GEOMETRY CALCULATOR\N WELCOME TO THE SHAPE CALCULATOR!") 
+
+#make a funtion for main 
+def main():
+    #print that this is the main funtion 
+    print("Main Function")
+    #make a while true loop 
+    while True:
+        #print the amount of shpaes created 
+
+        #Input the different actions the user can choose from called choice
+
+
+        #if choice equals 1 call the create shape funtion 
+
+        #elif choice equlas 2 call the view shapes funtion 
+
+        #elif choice equals 3 call the select shapes funtion
+
+        #elif choice equals 4 call the compare funtion
+
+        #elif chioce equlas 5 call the sort funtion
+
+        #elif choice equals 6 call the formula guide funtion 
+
+        #elif choice equals 7 break the loop 
+
+        #else print that the option is not there and continue
