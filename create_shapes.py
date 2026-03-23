e617e097c019ca058d9e0fdd37fb92ca523c36cc
edwingsantos <edwing.santos@ucas-edu.net>
2026-03-23 09:52:38 -0600

    started a lot of it, but ill try to finish the creating shape by tommorow

--- create_shapes.py
+++ create_shapes.py
@@ -1,6 +1,4 @@
 #creation for the shapes 
-
-
 #make a funtion called creation 
 
 #make a while true loop in it
@@ -8,3 +6,52 @@
     #print the avalible shapes numbered 
 
     #make the user input what their shape is 
+
+
+
+
+
+def circle_shape():
+
+
+    radius_shape = circle(input("What would you like your radius to be?"))
+
+    #make a class for circle 
+    class circle:
+        #defing init with self, shape, radius,area, perimeter, diameter and set those options to no 
+        def __init__(self, shape, radius, area, perimeter, diameter):
+            self.shpae =  shape
+            self.radius = radius 
+            self.area =  area
+    
+        def __setattr__(self):
+            return f"""Radius:{radius_shape}"""
+
+            
+circle_shape
+
+
+
+#make a class for rectangle
+
+
+
+
+#make a class for rectangle 
+
+
+
+
+
+
+#make a class for square
+
+
+
+
+
+
+
+
+
+#make a class for triangle 
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #ES 1rst geomarty calculator
 
+#import the files 
+import create_shapes
+import view_shapes
+import select_shapes
+import compare_shapes
+import sort_shapes
+import formula_guide
+
 #print the greating
 print("GEOMETRY CALCULATOR\N WELCOME TO THE SHAPE CALCULATOR!") 
 
@@ -10,22 +18,39 @@ def main():
     #make a while true loop 
     while True:
         #print the amount of shpaes created 
+    
+    
 
+
+
+
+    
         #Input the different actions the user can choose from called choice
-
-
+        choice = input("1: Create New Shape\n2: View All Shapes\n3: Select Shape\n4: Compare Shapes\n5: Sort Shapes\n:6 Formula Guide\n:7 Quit").strip()
         #if choice equals 1 call the create shape funtion 
-
+        if choice == "1":
+            create_shapes()
         #elif choice equlas 2 call the view shapes funtion 
-
+        elif choice == "2":
+            view_shapes()
         #elif choice equals 3 call the select shapes funtion
-
+        elif choice == "3":
+            select_shapes()
         #elif choice equals 4 call the compare funtion
-
+        elif choice == "4":
+            compare_shapes()
         #elif chioce equlas 5 call the sort funtion
-
+        elif choice == "5":
+            sort_shapes()
         #elif choice equals 6 call the formula guide funtion 
-
+        elif choice == "6":
+            formula_guide()
         #elif choice equals 7 break the loop 
-
+        elif choice == "7":
+            break 
         #else print that the option is not there and continue
+        else:
+            print("Option not avialable, please try agian.")
+            continue
+
+
